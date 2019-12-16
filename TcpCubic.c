6059ad170e3de3583d97302218c5ba8b1dd4a190
Nikhil Kumar <nikhildude281@gmail.com>
2019-12-16 13:42:35 +0530

    Create TcpCubic.cc

--- TcpCubic.c
+++ TcpCubic.c
@@ -11,8 +11,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
+ 
 
 #define NS_LOG_APPEND_CONTEXT \
   { std::clog << Simulator::Now ().GetSeconds () << " "; }
